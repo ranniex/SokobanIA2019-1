8eb1db2abb83aa3bbf0701c6ed9e2deb5f9a436c
ranniex <deporsam@hotmail.com>
2019-07-14 13:53:42 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,4 +1,4 @@
-var mapaInicial=['primer elemento'];
+var mapaInicial=[];
 
 const input = 
     document.querySelector('input[type="file"]')
@@ -7,11 +7,14 @@ const input =
         const reader = new FileReader()
         reader.onload = function () {
             const lines = reader.result.split('\n').map(function(line){
-                document.write(line);
-                mapaInicial.push[line];
+                mapaInicial.push(line);
+                if (line[1] == ','){
+                    document.write("Es posicion"+ "  "+ line[0]+ "\n");
+                }else{
+                    document.write("Es mapa" + "  " + line[0] + "\n");
+                }
+                
             })
-            document.write(mapaInicial[0]);
         }
         reader.readAsText(input.files[0]);
     },false)
-
