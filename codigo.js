49e6ce0c0f5898f838fc26a1e8873192d6097d97
ranniex <deporsam@hotmail.com>
2019-07-14 14:25:11 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,5 +1,7 @@
 var mapaInicial=[];
-var probando_comit = "test";
+var jugador=[];
+var caja1 = [], caja2 = [], caja3 = [];
+
 const input = 
     document.querySelector('input[type="file"]')
     input.addEventListener('change',function (e) {
@@ -9,12 +11,26 @@ const input =
             const lines = reader.result.split('\n').map(function(line){
                 mapaInicial.push(line);
                 if (line[1] == ','){
-                    document.write("Es posicion"+ "  "+ line[0]+ "\n");
-                }else{
-                    document.write("Es mapa" + "  " + line[0] + "\n");
-                }
-                
+                    if (jugador[0]==undefined){
+                        jugador[0]=line[0];
+                        jugador[1]=line[2];
+                        document.write("El jugador esta en: "+jugador[0]+","+jugador[1]+'\n');
+                    }else if (caja1[0] == undefined){
+                            caja1[0] = line[0];
+                            caja1[1] = line[2];
+                            document.write("la caja1 esta en: " + caja1[0] + "," + caja1[1] + '\n');
+                    }else if (caja2[0] == undefined){
+                            caja2[0] = line[0];
+                            caja2[1] = line[2];
+                            document.write("la caja2 esta en: " + caja2[0] + "," + caja2[1] + '\n');
+                    }else if (caja3[0] == undefined){
+                            caja3[0] = line[0];
+                            caja3[1] = line[2];
+                            document.write("la caja3 esta en: " + caja3[0] + "," + caja3[1] + '\n');
+                    }
+                }else{}
             })
         }
         reader.readAsText(input.files[0]);
     },false)
+
