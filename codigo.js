f569f663f7a34e47b413e1d60da2ca159b63cb06
ranniex <deporsam@hotmail.com>
2019-07-14 16:07:56 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -15,19 +15,15 @@ const input =
                     if (jugador[0]==undefined){
                         jugador[0]=line[0];
                         jugador[1]=line[2];
-                        document.write("El jugador esta en: "+jugador[0]+","+jugador[1]+'\n');
                     }else if (caja1[0] == undefined){
                             caja1[0] = line[0];
                             caja1[1] = line[2];
-                            document.write("la caja1 esta en: " + caja1[0] + "," + caja1[1] + '\n');
                     }else if (caja2[0] == undefined){
                             caja2[0] = line[0];
                             caja2[1] = line[2];
-                            document.write("la caja2 esta en: " + caja2[0] + "," + caja2[1] + '\n');
                     }else if (caja3[0] == undefined){
                             caja3[0] = line[0];
                             caja3[1] = line[2];
-                            document.write("la caja3 esta en: " + caja3[0] + "," + caja3[1] + '\n');
                     }
                 }else{
                     if(line.includes('X')){
@@ -42,8 +38,6 @@ const input =
                                 if (meta3[0] == undefined && (-1 != line.indexOf('X', meta2[1] + 1))) {
                                     meta3[0] = lineaActual;
                                     meta3[1] = line.indexOf('X',meta2[1]+1);
-                                    document.write("ENTROOOOOOOOOOOOOO");
-
                                 }
 
                             }
@@ -63,9 +57,7 @@ const input =
                 }
                 lineaActual+=1;
             })
-            document.write("La meta 1 esta en " + meta1[0] + "," + meta1[1] + '\n');
-            document.write("La meta 2 esta en " + meta2[0] + "," + meta2[1] + '\n');
-            document.write("La meta 3 esta en " + meta3[0] + "," + meta3[1] + '\n');
+
         }
         reader.readAsText(input.files[0]);
     },false)
