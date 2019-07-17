6d2950757d7b76b02dcfae574db639e514914b1e
invitado <invitado@s4pc3.eisc.univalle.edu.co>
2019-07-17 15:57:48 -0500

    modificando leertxt

--- main.py
+++ main.py
@@ -16,7 +16,38 @@ def leertxt(archivo):
             else:
                 cajaspos.append( [l[0],l[2]] )
         else:
-            tablero.append(l.replace('\n',''))
+            j = 0
+            temp = []
+            for a in l.replace('\n',''):
+                temp.append(a)
+                if a == 'X':
+                    metas.append( [i,j] )
+                j += 1
+            tablero.append(temp)
+        i += 1
+    f.close()
+    """for f in tablero:
+        j = 0
+        for c in f:
+            if c == 'X':
+                metas.append( [i,j] )
+            j += 1
+        i += 1"""
+
+"""def leertxt(archivo):
+    j = 0
+    i = 0
+    f = open(archivo,'r')
+    linea = f.readlines()
+    for l in linea:
+        if l[1]==',': #Corregir el problema de que la ultima linea sea = '\n'
+            if len(jugadorpos) == 0:
+                jugadorpos.append(l[0])
+                jugadorpos.append(l[2])
+            else:
+                cajaspos.append( [l[0],l[2]] )
+        else:
+            tablero.append(l)#l.replace('\n',''))
     f.close()
     for f in tablero:
         j = 0
@@ -24,11 +55,10 @@ def leertxt(archivo):
             if c == 'X':
                 metas.append( [i,j] )
             j += 1
-        i += 1
+        i += 1"""
 
 leertxt('nivel1.txt')
 
-print(tablero)
-print(jugadorpos)
-print(cajaspos)
-print(metas)
+def valid_move(f,c,dir):
+    if dir == 'U':
+        if tablero[f][c] == 'W'
